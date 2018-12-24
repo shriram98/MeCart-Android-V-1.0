c6e1ed9609e0367c93fa5d5697e6d345378a695a
peterjoe3132 <peterjoe3132@gmail.com>
2018-12-25 01:47:13 +0530

    modified:   App.js 	new file:   For routing

--- MeCart-Android/App.js
+++ MeCart-Android/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import AppNav from './AppNavigator';
+import {Button} from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Button title="button"></Button>
-      </View>
+      <AppNav />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
