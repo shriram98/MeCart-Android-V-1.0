c279983814a5a3dfb86c4f7413e53926f352bdb4
peterjoe3132 <peterjoe3132@gmail.com>
2018-12-25 01:52:09 +0530

    new file:   BuyPage with routing 	new file:   SellPage with routing

---
+++ MeCart-Android/components/BuyPage/BuyPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Buy extends React.Component {
+  render() {
+    return (
+      <View>
+    <Text>
+    This is the Buy page
+    </Text>
+   
+    </View>
+    );
+  }
+}
